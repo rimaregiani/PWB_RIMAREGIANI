c374fd57ee5fa401ba04f06bdeb9d375ad8d4484
rimaregiani <regianirima@gmail.com>
2022-02-06 00:17:55 +0700

    add folder phpmvc

---
+++ phpmvc/app/init.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'core/App.php'; //sebagai class App
+require_once 'core/Controller.php'; //sebagai class Controller
+require_once 'core/Constants.php'; //sebagai class Controller
---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-3">About Me</h1>
+    <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Rima Regiani" width="150" class="mb-3 rounded-circle shadow">
+    <p>Halo nama saya <?= $data['nama']; ?>, dan pekerjaan saya adalah <?= $data['pekerjaan']; ?>.</p>
+</div>
